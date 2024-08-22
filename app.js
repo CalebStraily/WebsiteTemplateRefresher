2390409aeab8d55c279d414390d8855895962967
Cbstray <calebstraily01@gmail.com>
2024-08-22 15:40:00 -0500

    Event Exercises
    Added a series of code solutions for Event Listener exercises provided by MCA (reference picture can be located within "stylesheets/media/Exercise Reference").

--- app.js
+++ app.js
@@ -71,4 +71,80 @@ function stylingText()
     }
 }
 
-serviceImageOne.addEventListener("click", stylingText);
+//sets the font size of each service subtext
+for (let i = 0; i < serviceColumns.length; i++)
+{
+    serviceColumns[i].style.fontSize = "26px";
+}
+
+
+
+//EVENT EXERCISES
+
+//Ex. 1: Revert "Pizza" text back to its original word IN JAVASCRIPT
+
+//sets web service subtext back to its original word and resets the font size
+serviceColumns[2].innerHTML = "Design";
+
+//Ex. 2: If the smartphone icon on the services row is clicked, change the nav text to BLUE
+
+//declares a variable to store the element gotten by id for the image that needs to be clicked
+let smartphoneImage = document.getElementById("smartphoneImage");
+
+//adds an event lister that will run the onSmartphoneImagePress function when the image element is clicked
+smartphoneImage.addEventListener("click", onSmartphoneImagePress)
+
+//gets the elements that need to change colors, stores them in a local variable, and changes their font color to blue
+function onSmartphoneImagePress()
+{
+    let serviceMobile = document.getElementById("serviceMobile");
+    let serviceSubtext = document.getElementById("serviceSubtext");
+
+    serviceMobile.style.color = "blue";
+    serviceSubtext.style.color = "blue";
+}
+
+//Ex. 3: Output "Ima Mr. Portfolio to the console when the word 'PORTFOLIO' in 'RECENT FROM PORTFOLIO' is hovered over with the mouse
+
+//set the word that needs to be hovered over to a local variables
+let portfolioWord = document.getElementById("portfolioHeaderWord");
+
+//add the event listener to check for when the mouse is hovering over the word 'PORTFOLIO'
+portfolioWord.addEventListener("mouseover", onPortfolioHover);
+
+//console out the statement
+function onPortfolioHover()
+{
+    console.log("Ima Mr. Portfolio");
+}
+
+
+//Ex. 4: Change all the green backgrounds of the subtext links to purple
+
+//queries all the portfolio links
+let portfolioLinks = document.querySelectorAll(".portfolio a");
+
+//iterates for each portfolio link and changes the background color to purple
+for (let i = 0; i < portfolioLinks.length; i++)
+{
+    let element = portfolioLinks[i];
+
+    element.style.backgroundColor = "purple";
+}
+
+
+//Ex. 5: When the Moo Minicard Image is clicked, change the subheading font to 60px
+
+//gets the image element by id
+let mooMinicardImage = document.getElementById("mooMinicardsImage");
+
+//adds an on click event listener for that element 
+mooMinicardImage.addEventListener("click", changeSubheaderFont);
+
+//gets the subheader element by id and changes the font size to 60px
+function changeSubheaderFont()
+{
+    let element = document.getElementById("portfolioSubheader");
+
+    element.style.fontSize = "60px";
+}
