0d8d8a160de5aefe7b7dad74201caa58fa2b7322
Cbstray <calebstraily01@gmail.com>
2024-08-22 14:02:01 -0500

    JavaScript DOM Events
    Created an app.js with a series of code lines that call specific elements and changes their style based on event listeners.

---
+++ app.js
@@ -0,0 +1,57 @@
+//This will grab the entire element by the ID name
+let serviceHeading = document.getElementById("serviceAmazing");
+
+console.log(serviceHeading);
+
+//grabbing the id by multiple elements by className
+let serviceElements = document.getElementsByClassName("serviceIcons");
+
+console.log(serviceElements);
+
+//grabbing the element by css selection
+let serviceSupport = document.querySelector(".amazingIcon h4");
+
+console.log(serviceSupport);
+
+//grabbling all the elements by css selection
+let serviceColumns = document.querySelectorAll(".serviceIcons h4");
+
+console.log(serviceColumns);
+
+//if we want to add text use .innerHTML
+serviceHeading.innerHTML = "AMAZING";
+
+//changing the text for a specific index in a NODECollection
+serviceColumns[2].innerHTML = "Pizza";
+
+styleCenter();
+
+function styleCenter()
+{
+    for (let i = 0; i < serviceColumns.length; i++)
+    {
+        const element = serviceColumns[i];
+    
+        element.style.fontSize = "60px";
+    }
+}
+
+//EVENT LISTENERS
+let serviceImageOne = document.querySelector(".amazingIcon img");
+
+console.log(serviceImageOne);
+
+serviceImageOne.addEventListener("click", onImagePress);
+
+function onImagePress()
+{
+    console.log("Image pressed.");
+
+    let element = serviceColumns[0];
+
+    element.innerHTML = "Bruh Moment";
+    element.style.color = "purple";
+    element.style.fontSize = "69px";
+
+    console.log(`Service subtext changed to: ${serviceColumns[0].innerHTML}`);
+}
